d7b31d40976dabe1a55970fec6991bbd0c4c60f8
Alex <luckyone@tutamail.com>
2022-05-02 23:33:31 -0400

    Form Wave Animation

--- Blurry Loading/script.js
+++ Blurry Loading/script.js
@@ -1,20 +1,20 @@
-const loadText = document.querySelector(".loading-text");
-const bg = document.querySelector(".bg");
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
 
-let load = 0;
-let int = setInterval(blurring, 30);
+let load = 0
+let int = setInterval(blurring, 30)
 const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
 
 function blurring() {
-  load++;
+  load++
 
   if (load > 99) {
-    clearInterval(int);
+    clearInterval(int)
   }
 
-  loadText.innerText = `${load}%`;
-  loadText.style.opacity = scale(load, 0, 100, 1, 0);
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
